c7fd6c76d24aa7eed56f94ecba889b100e271086
Israel Del Toro <ihernandez@automatyco.com>
2022-06-22 23:27:41 -0500

    dividiendo layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,65 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="keywords"
-          content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, ample admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template"
-        />
-        <meta
-          name="description"
-          content="Ample is powerful and clean admin dashboard template, inpired from Google's Material Design"
-        />
-        <meta name="robots" content="noindex,nofollow" />
-        <title>Ample Template by WrapPixel</title>
-        <link
-          rel="canonical"
-          href="https://www.wrappixel.com/templates/ampleadmin/"
-        />
-        <!-- Favicon icon -->
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="https://demos.wrappixel.com/premium-admin-templates/bootstrap/ample-bootstrap/package/assets/images/favicon.png"
-        />
-        <link
-          rel="stylesheet"
-          href="{{url('assets/libs/apexcharts/dist/apexcharts.css')}}"
-        />
-        <!-- Custom CSS -->
-        <link
-          href="../../assets/libs/fullcalendar/dist/fullcalendar.min.css"
-          rel="stylesheet"
-        />
-        <link href="../../assets/extra-libs/calendar/calendar.css" rel="stylesheet" />
-        <!-- needed css -->
-        <link href="dist/css/style.min.css" rel="stylesheet" />
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-      </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        @yield('head')
+    </head>
+    <body>
+        @yield('preloader')
+        <div id="main-wrapper">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @yield('topbar')
+            @yield('lefsidebar')
+            @yield('content')
+            @yield('scripts')
         </div>
     </body>
 </html>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,32 @@
+@section('scripts')
+<!-- -------------------------------------------------------------- -->
+    <!-- All Jquery -->
+    <!-- -------------------------------------------------------------- -->
+    <script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../../assets/extra-libs/taskboard/js/jquery-ui.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- apps -->
+    <script src="../../dist/js/app.min.js"></script>
+    <script src="../../dist/js/app.init.sidebar.js"></script>
+    <script src="../../dist/js/app-style-switcher.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="../../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../../assets/extra-libs/sparkline/sparkline.js"></script>
+    <!--Wave Effects -->
+    <script src="../../dist/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="../../dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../../dist/js/feather.min.js"></script>
+    <script src="../../dist/js/custom.min.js"></script>
+    <!-- This Page JS -->
+    <script src="../../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="../../dist/js/pages/dashboards/dashboard1.js"></script>
+    <script src="../../assets/libs/moment/min/moment.min.js"></script>
+    <script src="../../assets/libs/fullcalendar/dist/fullcalendar.min.js"></script>
+    <script src="../../dist/js/pages/calendar/cal-init.js"></script>
+    <script>
+      $("#calendar").fullCalendar("option", "height", 650);
+    </script>
+@endsection
